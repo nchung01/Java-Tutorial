80851739a704035ba9b6f3bf2c5e3aa323e2cae3
Nate <Nate@DESKTOP-EU9DENN>
2022-01-25 13:55:38 -0500

    Initial commit

---
+++ src/javaapplication16/JavaApplication16.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication16;
+
+/**
+ *
+ * @author Nate
+ */
+public class JavaApplication16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Git programming");
+    }
+    
+}
