9012b415190eda4c2a7cb91a588ddde546cb7ea6
Nate <Nate@DESKTOP-EU9DENN>
2022-01-25 13:58:28 -0500

    Revert "Initial commit"
    This reverts commit f7a33c5dca447de9cbc9367d60eca65f18c71eac.

--- src/javaapplication16/JavaApplication16.java
+++ src/javaapplication16/JavaApplication16.java
@@ -14,7 +14,7 @@ public class JavaApplication16 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Welcome to Git");
+        System.out.println("Welcome to Git programming");
     }
     
 }
